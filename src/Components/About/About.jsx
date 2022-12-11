6b8aa80248d934ffd6c625be7a07009f3e8b6ac3
Subham Das <subhamdassneha@gmail.com>
2022-12-12 02:28:37 +0530

    about section added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import ME from '../../assets/me-about.jpg'
 
+import {FaUserGraduate, FaBook} from 'react-icons/fa'
+import {AiFillProject} from 'react-icons/ai'
+
+import './About.css'
 
 const About = () => {
   return (
@@ -9,7 +14,39 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-          
+          <div className="about__me-image">
+            <img src={ME} alt="About me " />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaUserGraduate className="about__icons"/>
+              <h4>Education</h4>
+              <small>B.Tech</small>
+              <small>Electrical Engineering</small>
+              <small>NIT Rourkela, 2024</small>
+            </article>
+            <article className="about__card">
+              <AiFillProject className="about__icons"/>
+              <h4>Projects</h4>
+              <small>5 +</small>
+              <small>Frontend & backend</small>
+            </article>
+            <article className="about__card">
+              <FaBook className="about__icons"/>
+              <h4>Interships</h4>
+              <small>None</small>
+            </article>
+            
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam maiores atque, 
+            suscipit temporibus distinctio quibusdam voluptates nemo repellat neque placeat 
+            animi dignissimos officia sunt ipsam dicta commodi aliquid accusantium veritatis!
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaHome, FaUserAlt, FaBook } from "react-icons/fa";
 import { HiCollection } from "react-icons/hi";
 import { BsFillChatRightTextFill } from "react-icons/bs";
+import { useEffect } from "react";
 
 import "./Nav.css";
 
@@ -13,32 +14,40 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState("home");
 
   /* Currently using normal javascript way to change navbar, since I don't know how to properly use userefs and useEffect */
-  const changeNav = ()=>{
-    const scrlPos = window.scrollY;
-    const wHeight = window.innerHeight;
-    if(scrlPos < wHeight )
-    {
-      setActiveNav('home');
-    }
-    else if(scrlPos >= 1.1*wHeight-10 && scrlPos < 2.2*wHeight - 10)
-    {
-      setActiveNav('about');
-    }
-    else if(scrlPos >= 2.2*wHeight-10 && scrlPos < 3.3*wHeight - 10)
-    {
-      setActiveNav('exp');
-    }
-    else if(scrlPos >= 3.3*wHeight-10 && scrlPos < 4.4*wHeight - 10)
-    {
-      setActiveNav('portfolio');
-    }
-    else if(scrlPos >= 4.4*wHeight-10 && scrlPos < 5.5*wHeight)
-    {
-      setActiveNav('contact');
-    }
-  }
 
-  window.addEventListener('scroll', changeNav);
+  useEffect(() => {
+    const changeNav = ()=>{
+      const scrlPos = window.scrollY;
+      const wHeight = window.innerHeight;
+      if(scrlPos < wHeight )
+      {
+        setActiveNav('home');
+      }
+      else if(scrlPos >= 1.1*wHeight-10 && scrlPos < 2.2*wHeight - 10)
+      {
+        setActiveNav('about');
+      }
+      else if(scrlPos >= 2.2*wHeight-10 && scrlPos < 3.3*wHeight - 10)
+      {
+        setActiveNav('exp');
+      }
+      else if(scrlPos >= 3.3*wHeight-10 && scrlPos < 4.4*wHeight - 10)
+      {
+        setActiveNav('portfolio');
+      }
+      else if(scrlPos >= 4.4*wHeight-10 && scrlPos < 5.5*wHeight)
+      {
+        setActiveNav('contact');
+      }
+    }
+      
+    window.addEventListener('scroll', changeNav);
+    
+    return () => {
+      window.removeEventListener('scroll', changeNav);
+    }
+  }, []);
+
   return (
     <nav>
       {/* HOME */}
